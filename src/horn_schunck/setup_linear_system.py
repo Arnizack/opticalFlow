Derivative Fix

src/horn_schunck/setup_linear_system.py
@@ -91,7 +91,7 @@ def setup_diagonals_image(width,height,I_xx,I_xy,I_yy,alpha):
     diags = [R,middle,R]
     return (diags,offset)
 
-def setup_linear_system(first_image,second_image,second_image_derivative,alpha):
+def setup_linear_system(first_image,second_image,first_image_derivative,second_image_derivative,alpha):
 
     channel_count = first_image.shape[0]
     height = first_image.shape[2] #switched height,width
@@ -105,8 +105,25 @@ def setup_linear_system(first_image,second_image,second_image_derivative,alpha):
     I_xt = np.zeros(shape=(height * width), dtype=float)
     I_yt = np.zeros(shape=(height * width), dtype=float)
     for channel_idx in range(channel_count):
-        channel_x = second_image_derivative[channel_idx][1].flatten()
-        channel_y = second_image_derivative[channel_idx][0].flatten()
+        channel_first_x = first_image_derivative[channel_idx][1].flatten()
+        channel_first_y = first_image_derivative[channel_idx][0].flatten()
+
+        channel_second_x = second_image_derivative[channel_idx][1].flatten()
+        channel_second_y = second_image_derivative[channel_idx][0].flatten()
+        #deriviative blending: see An Improved Algorithm for TV-L1 Optical Flow
+        b=0.4
+        channel_x = channel_first_x*b+channel_second_x*(1-b)
+        channel_y = channel_first_y*b+channel_second_y*(1-b)
+
+        """
+        print("channel_x, channel_y")
+        plt.figure()
+        plt.imshow(channel_x.reshape(height,width))
+        plt.figure()
+        plt.imshow(channel_y.reshape(height,width))
+        plt.show()
+        """
+
         channel_t = second_image[channel_idx] - first_image[channel_idx]
         channel_t = channel_t.flatten()
         I_xx += channel_x ** 2
@@ -116,11 +133,21 @@ def setup_linear_system(first_image,second_image,second_image_derivative,alpha):
         I_yt += channel_y * channel_t
 
     channel_count_squared = channel_count**2
+
     I_xx/=channel_count_squared
     I_xy /= channel_count_squared
     I_yy /= channel_count_squared
     I_xt /= channel_count_squared
     I_yt /= channel_count_squared
+    """
+    print("I_xx, I_xy, I_yy, I_xt, I_yt")
+    plt.figure()
+    plt.imshow(I_xx.reshape(width,height))
+    plt.figure()
+    plt.imshow(I_xy.reshape(width,height))
+    plt.figure()
+    plt.imshow(I_yy.reshape(width,height))
+    plt.show()"""
 
     diags1,offset1 = setup_diagonals_image(width,height,I_xx,I_xy,I_yy,alpha)
 

src/horn_schunck/solve_layer.py
@@ -16,12 +16,13 @@ class SolverSettings:
 
 
 
-def solve_layer(first_frame,second_frame,
+def solve_layer(first_frame,second_frame,first_frame_derivative,
                 second_frame_derivative, initial_flow_field, solver_settings):
     """
 
     :param first_frame: np.array(float) shape = (ColorChannel,Height,Width)
     :param second_frame: np.array(float) shape = (ColorChannel,Height,Width)
+    :param first_frame_derivative: np.array(float) shape = (ColorChannel,Derivative_Direction,Height,Width)
     :param second_frame_derivative: np.array(float) shape = (ColorChannel,Derivative_Direction,Height,Width)
     :param initial_flow_field: np.array(float) (Flow_Direction, Height,Width)
     :param solver_settings: SolverSettings
@@ -33,7 +34,7 @@ def solve_layer(first_frame,second_frame,
     plt.figure()
     show_image(second_frame_warped)
 
-    A,b = setup_linear_system(first_frame,second_frame,second_frame_derivative_warped,solver_settings.alpha)
+    A,b = setup_linear_system(first_frame,second_frame,first_frame_derivative,second_frame_derivative_warped,solver_settings.alpha)
 
     print("Lg start")
     start = time()

src/horn_schunck/tests/solve_layer_test.py
@@ -5,22 +5,20 @@ from time import time
 
 from src.utilities.image_access import open_image, show_image
 from src.utilities.image_derivative import differentiate_image
-from  src.utilities.flow_field_helper import show_flow_field,read_flow_field
+from  src.utilities.flow_field_helper import show_flow_field,read_flow_field,show_flow_field_arrow
 from src.utilities.image_pyramid import downscale_image
-
+from src.utilities.warp_grid import warp_image
 
 
 def test_layer1(img1,img2):
 
 
-    img1 = downscale_image(img1, 0.3)
-    img2 = downscale_image(img2, 0.3)
+    #img1 = downscale_image(img1, 0.3)
+    #img2 = downscale_image(img2, 0.3)
 
     der_img2 = differentiate_image(img2)
-    plt.imshow(der_img2[0,0])
-    plt.figure()
-    plt.imshow(der_img2[0, 1])
-    plt.show()
+    der_img1 = differentiate_image(img1)
+
     settings = SolverSettings()
     settings.alpha=15
     init_flow = np.zeros(shape=(2, img1.shape[1], img1.shape[2]))
@@ -29,14 +27,25 @@ def test_layer1(img1,img2):
     height = img2.shape[1]
     start = time()
     for iter in range(1):
-        flow = solve_layer(img1,img2,der_img2,init_flow,settings)
+        flow = solve_layer(img1,img2,der_img1,der_img2,init_flow,settings)
         init_flow+=flow
     print("Solve Layer time: ", time() - start)
     plt.show()
-    show_flow_field(np.array([init_flow[1],init_flow[0]]),height,width)
-    ref_flow = read_flow_field(r"..\..\..\resources\eval-twoframes-groundtruth\Dimetrodon\flow10.flo")
-    show_flow_field(ref_flow,ref_flow.shape[1],ref_flow.shape[2])
+    plt.title("Y Flow")
+    plt.imshow(init_flow[0])
+    plt.figure()
+    plt.title("X Flow")
+    plt.imshow(init_flow[1])
     plt.show()
+
+    print("Warped result")
+    img2_warped = warp_image(img2, np.array([init_flow[0],init_flow[1]]))
+    show_image(img2_warped)
+    plt.show()
+    #show_flow_field_arrow(np.array([init_flow[1],init_flow[0]]),height,width)
+    #plt.show()
+    show_flow_field(np.array([init_flow[0],init_flow[1]]),height,width)
+
 
 def test_layer2():
     img1 = open_image(r"..\..\..\resources\calibration\foatage1\frame10.jpg")
@@ -65,8 +74,17 @@ if __name__ == '__main__':
     #test_setup_linear_system()
     img1 = open_image(r"..\..\..\resources\eval-twoframes\Dimetrodon\frame10-gray.png")
     img2 = open_image(r"..\..\..\resources\eval-twoframes\Dimetrodon\frame11-gray.png")
+    #img1 = open_image(r"..\..\..\resources\eval-twoframes\syntetisch\frame10.jpg")
+    #img2 = open_image(r"..\..\..\resources\eval-twoframes\syntetisch\frame11.jpg")
+
     img1 = img1[[0]]
     img2 = img2[[0]]
     #img1 = open_image(r"..\..\..\resources\eval-twoframes\Dimetrodon\frame10.png")
     #img2 = open_image(r"..\..\..\resources\eval-twoframes\Dimetrodon\frame11.png")
+    show_image(img1)
+    plt.show()
     test_layer1(img1,img2)
+
+    ref_flow = read_flow_field(r"..\..\..\resources\eval-twoframes-groundtruth\Dimetrodon\flow10.flo")
+    show_flow_field(ref_flow, ref_flow.shape[1], ref_flow.shape[2])
+    plt.show()

src/utilities/image_derivative.py
@@ -1,4 +1,5 @@
-from scipy.ndimage.filters import convolve1d,gaussian_filter
+from scipy.ndimage.filters import gaussian_filter,median_filter
+from scipy.signal import convolve2d
 import numpy as np
 
 def differentiate_matrix(matrix2d):
@@ -10,11 +11,12 @@ def differentiate_matrix(matrix2d):
     # Same Kernel as in A Quantitative Analysis of Current Practices in Optical Flow Estimation
     # and the Principles behind Them; Deqing Sun Stefan Roth Michael J. Black
     #kernel = 1/12 * np.array([-1,8,0,-8,1])
-    kernel =  np.array([-1, 8, 0, -8, 1])/12
-    y_derivative = convolve1d(matrix2d,kernel,axis=0)
-    x_derivative = convolve1d(matrix2d, kernel, axis=1)
-    y_derivative=gaussian_filter(y_derivative,sigma=1)
-    x_derivative = gaussian_filter(x_derivative, sigma=1)
+    kernel_x =  np.array([[-1, 8, 0, -8, 1]])/12
+    kernel_y = np.array([[-1], [8], [0], [-8], [1]])/12
+    y_derivative = convolve2d(matrix2d,kernel_y,mode="same", boundary='symm')
+    x_derivative = convolve2d(matrix2d, kernel_x,mode="same", boundary='symm')
+    #y_derivative=median_filter(y_derivative,size=3)
+    #x_derivative = median_filter(x_derivative, size=3)
     return np.array([y_derivative,x_derivative],copy=False)
 
 def differentiate_image(np_img):