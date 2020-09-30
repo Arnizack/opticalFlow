0e88f2243c70f116c5fda4c9bad0ac0ddcbcc019
Bucktron <Jo.Men@outlook.de>
2020-09-30 15:48:20 +0200

    ILinearSolver changed Matirx

--- src/OptFlowCpp/core/solver/ILinearSolver.h
+++ src/OptFlowCpp/core/solver/ILinearSolver.h
@@ -1,16 +1,17 @@
 #pragma once
-#include "..\IContainer.h"
+#include "..\IArray.h"
+#include "..\linalg\ILinearOperator.h"
 #include <memory>
 namespace core
 {
 	namespace solver
 	{
-		template<class InnerTyp, class SettingsTyp>
+		template<class InnerTyp, size_t DimCount, class SettingsTyp>
 		class ILinearSolver
 		{
 		public:
-			using PtrMatrix = std::shared_ptr<IContainer<InnerTyp>>;
-			using PtrVector = std::shared_ptr<IContainer<InnerTyp>>;
+			using PtrVector = std::shared_ptr<IArray<InnerTyp, 1>>;
+			using PtrMatrix = std::shared_ptr<linalg::ILinearOperator<std::shared_ptr<IArray<InnerTyp, DimCount>>, PtrVector, PtrVector>>;
 
 			virtual PtrVector Solve(const PtrMatrix input_matrix, const PtrVector input_vector) = 0; Solve(const PtrMatrix input_matrix, const PtrVector input_vector) = 0;
 			virtual PtrVector Solve(const PtrMatrix input_matrix, const PtrVector input_vector, const PtrMatrix preconditioner_matrix, const PtrVector initial_guess) = 0;
