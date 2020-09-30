a61e19cafcbae65035320ddf3d760d80b6ebb983
Bucktron <Jo.Men@outlook.de>
2020-09-30 15:55:33 +0200

    ILinearSolver Matrix

--- src/OptFlowCpp/core/solver/ILinearSolver.h
+++ src/OptFlowCpp/core/solver/ILinearSolver.h
@@ -11,7 +11,7 @@ namespace core
 		{
 		public:
 			using PtrVector = std::shared_ptr<IArray<InnerTyp, 1>>;
-			using PtrMatrix = std::shared_ptr<linalg::ILinearOperator<std::shared_ptr<IArray<InnerTyp, DimCount>>, PtrVector, PtrVector>>;
+			using PtrMatrix = std::shared_ptr<linalg::ILinearOperator<PtrVector, PtrVector>>;
 
 			virtual PtrVector Solve(const PtrMatrix input_matrix, const PtrVector input_vector) = 0; Solve(const PtrMatrix input_matrix, const PtrVector input_vector) = 0;
 			virtual PtrVector Solve(const PtrMatrix input_matrix, const PtrVector input_vector, const PtrMatrix preconditioner_matrix, const PtrVector initial_guess) = 0;
