a759d5d398cb8ed2d3d68a3c79b0462134a1be3f
Arnizack <42911677+Arnizack@users.noreply.github.com>
2020-10-11 11:48:42 +0200

    Array fix

--- src/OptFlowCpp/cpu_backend/Array.h
+++ src/OptFlowCpp/cpu_backend/Array.h
@@ -11,31 +11,38 @@ namespace cpu_backend
 	public:
 		Array() = default;
 
-		Array(std::array<const size_t, DimCount> shape, const size_t& size, const InnerTyp *const src) : core::IArray<InnerTyp, DimCount>(shape), _size(size), _data(std::vector<InnerTyp>(size))
+		Array(std::array<const size_t, DimCount> shape, const InnerTyp *const src) : 
+			core::IArray<InnerTyp, DimCount>(shape), _size(1), _data(std::vector<InnerTyp>())
 		{
-			std::copy_n(src, _size, _data.data());
-		}
 
-		Array(std::array<const size_t, DimCount> shape, const InnerTyp* const src) : core::IArray<InnerTyp, DimCount>(shape), _size(1), _data(std::vector<InnerTyp>(1))
-		{
-			for (size_t i = 0; i < DimCount; i++)
+			for (const size_t& dim : shape)
 			{
-				_size *= shape[i];
+				_size *= dim;
 			}
-
 			_data = std::vector<InnerTyp>(_size);
+
 			std::copy_n(src, _size, _data.data());
 		}
 
-		Array(std::array<const size_t, DimCount> shape, const size_t& size, const InnerTyp& single_value) : core::IArray<InnerTyp, DimCount>(shape), _size(size), _data(std::vector<InnerTyp>(size))
+
+		Array(std::array<const size_t, DimCount> shape, const InnerTyp& single_value) : 
+			core::IArray<InnerTyp, DimCount>(shape), _size(1), _data(std::vector<InnerTyp>())
 		{
-			for (size_t i = 0; i < size; i++)
+
+			for (const size_t& dim : shape)
+			{
+				_size *= dim;
+			}
+			_data = std::vector<InnerTyp>(_size);
+			for (size_t i = 0; i < _size; i++)
 			{
 				_data[i] = single_value;
 			}
+
 		}
 
-		Array(const std::array<const size_t, DimCount>& shape) : core::IArray<InnerTyp, DimCount>(shape), _size(1), _data(std::vector<InnerTyp>())
+		Array(const std::array<const size_t, DimCount>& shape) : 
+			core::IArray<InnerTyp, DimCount>(shape), _size(1), _data(std::vector<InnerTyp>())
 		{
 			for(const size_t& dim : shape)
 			{
