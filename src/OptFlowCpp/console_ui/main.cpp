12a74f0f8b7f6c17a96a1180b59adf675d56f552
Bucktron <Jo.Men@outlook.de>
2020-11-13 13:50:32 +0100

    temp

--- src/OptFlowCpp/console_ui/main.cpp
+++ src/OptFlowCpp/console_ui/main.cpp
@@ -1,4 +1,65 @@
-int main()
+#include <boost/program_options.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[])
 {
-	return -1;
+	namespace bpo = boost::program_options;
+
+	std::string input_file_1;
+	std::string input_file_2;
+	std::string output_path;
+
+	//Setting up all posible Arguments
+	bpo::options_description generic("Generic options");
+	generic.add_options()
+		("help", "produce help message");
+
+	bpo::options_description mandatory("Mandatory");
+	mandatory.add_options()
+		("input-file1,1", bpo::value< std::string>(&input_file_1), "first input file")
+		("input-file2,2", bpo::value< std::string>(&input_file_2), "second input file")
+		("output-path,O", bpo::value< std::string>(&output_path), "Output path");
+
+	bpo::options_description cmdline("All Options");
+	cmdline.add(generic).add(mandatory);
+
+	//Parsing Comandline
+	bpo::variables_map vm;
+	bpo::store(bpo::parse_command_line(argc, argv, cmdline), vm);
+	bpo::notify(vm);
+
+	//Checking what is set
+	if (vm.count("help"))
+	{
+		std::cout << cmdline << '\n';
+		return 1;
+	}
+
+	if (!vm.count("input-file1"))
+	{
+		std::cout << "See --help for must set variables\n";
+		return 2;
+	}
+
+	if (!vm.count("input-file2"))
+	{
+		std::cout << "See --help for must set variables\n";
+		return 2;
+	}
+
+	if (!vm.count("output-path"))
+	{
+		std::cout << "See --help for must set variables\n";
+		return 2;
+	}
+
+	//Test output
+	std::cout << "Include File 1 is: " << input_file_1 << '\n';
+	std::cout << "Include File 2 is: " << input_file_2 << '\n';
+	std::cout << "Output Path is: " << output_path << '\n';
+
+	return 0;
 }
