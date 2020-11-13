74c104830609582cbaf06790a5ab5c01ca3ada58
Arnizack <42911677+Arnizack@users.noreply.github.com>
2020-11-13 21:26:34 +0100

    Downscale

---
+++ src/OptFlowCpp/cpu_backend/image/inner/DownScaleGaussianGrayScale.h
@@ -0,0 +1,55 @@
+#pragma once
+#include"filters/Gaussian1DFilter.h"
+#include<memory>
+
+
+namespace cpu_backend
+{
+
+	namespace _inner
+	{
+		template<class T>
+		T ComputeStandardDeviation(int src_length, int dst_length)
+		{
+			T scale_factor = (T)dst_length / (T)src_length;
+			return 1 / sqrt(2 * scale_factor);
+		}
+	}
+
+	template<class T>
+	void DownScaleGaussianGrayScale(T* image, int width, int height, int dest_width, int dest_height, T* destination)
+	{
+		T scale_factor_width = (T)dest_width / (T) width;
+		T scale_factor_height = (T)dest_height / (T)height;
+		auto temp_x_blured = std::make_unique<std::vector<T>>(width * height);
+		auto temp_xy_blured = std::make_unique<std::vector<T>>(width * height);
+
+		T* ptr_temp_x_blured = temp_x_blured->data();
+		T* ptr_temp_xy_blured = temp_xy_blured->data();
+
+		T std_deviation_x = _inner::ComputeStandardDeviation<T>(width, dest_width);
+		T std_deviation_y = _inner::ComputeStandardDeviation<T>(height, dest_height);
+
+		Gaussian1DFilter<T, Direction::X, Padding::SYMMETRIC>(image, width, height, std_deviation_x, ptr_temp_x_blured);
+		Gaussian1DFilter<T, Direction::Y, Padding::SYMMETRIC>(ptr_temp_x_blured, width, height, std_deviation_y, ptr_temp_xy_blured);
+
+		T dest_to_src_scaler_x = (T)width / (T)dest_width;
+
+		T dest_to_src_scaler_y = (T)height / (T)dest_height;
+
+		T result = 0;
+
+		for(int dest_y = 0; dest_y < dest_height; dest_y++)
+		{
+			
+			int src_y = round( dest_to_src_scaler_y * (T)dest_y + 0.25* dest_to_src_scaler_y);
+			for (int dest_x = 0; dest_x < dest_width; dest_x++)
+			{
+				int src_x = round(dest_to_src_scaler_x * (T)dest_x + 0.25 * dest_to_src_scaler_x);
+				result = ptr_temp_xy_blured[src_y * width + src_x];
+				destination[dest_y * dest_width + dest_x] = result;
+			}
+		}
+
+	}
+}
---
+++ src/OptFlowCpp/cpu_backend/image/inner/filters/Gaussian1DFilter.h
@@ -0,0 +1,49 @@
+#pragma once
+#include"../convolution1D.h"
+#include<vector>
+
+#include <cmath>
+
+#define M_PI 3.14159265358979323846
+
+namespace cpu_backend
+{
+
+	namespace _inner
+	{
+		template<class T>
+		std::vector<T> CreateKernel(long double standard_deviation)
+		{
+
+			size_t kernel_length = ceil(standard_deviation) * 6 + 1; 
+			std::vector<T> kernel(kernel_length);
+			
+			const long double sqrt2Pi = sqrt((long double)2.0 * M_PI);
+
+			//T scaler = 1.0 / (standard_deviation * sqrt2Pi);
+
+			//T x_scaler = 1.0 / (2 * standard_deviation * standard_deviation);
+
+			long double scaler = (standard_deviation * sqrt2Pi);
+
+			long double x_scaler = (2 * standard_deviation * standard_deviation);
+
+			for (int i = 0; i < kernel_length; i++)
+			{
+				long double x = (long double)i - floor(kernel_length / 2);
+				kernel[i] =  exp(-(x * x) / x_scaler) / scaler;
+			}
+
+			return kernel;
+		}
+	}
+	template<class T, Direction _Direction, Padding _Padding>
+	void Gaussian1DFilter(T* image, int width, int height, double standard_deviation, T* destination)
+	{
+		std::vector<T> kernel = _inner::CreateKernel<T>(standard_deviation);
+		Convolute1D<T, _Padding, _Direction>(image, width, height, kernel.data(), kernel.size(), destination);
+
+	}
+
+
+}
