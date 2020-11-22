cd982950a2100e7420fd882fa12947b2b16649df
Bucktron <Jo.Men@outlook.de>
2020-11-22 23:01:30 +0100

    using correct main

--- src/OptFlowCpp/console_ui/main.cpp
+++ src/OptFlowCpp/console_ui/main.cpp
@@ -32,54 +32,54 @@ int main(int argc, char* argv[])
 	* Christian 2 Image: "H:\\dev\\opticalFlow\\Prototyp\\Version 2\\opticalFlow\\resources\\eval-twoframes\\Dimetrodon\\frame11.png";
 	*/
 
-	//core::Logger::Init();
-	//debug::ImageLogger::Init(
-	//	"E:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_images"
-	//	/*"H:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_images"*/,
-	//	"E:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_flow"
-	//	/*"H:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_flow"*/);
+	core::Logger::Init();
+	debug::ImageLogger::Init(
+		"E:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_images"
+		/*"H:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_images"*/,
+		"E:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_flow"
+		/*"H:\\dev\\opticalFlow\\optFlowCpp\\opticalFlow\\src\\OptFlowCpp\\bin\\debug_flow"*/);
 
-	//OF_LOG_INFO("Start Logger");
+	OF_LOG_INFO("Start Logger");
 
-	//std::string first_img_path = "..\\..\\..\\resources\\eval-twoframes\\Dimetrodon\\frame10.png"; 
-	//std::string second_img_path = "..\\..\\..\\resources\\eval-twoframes\\Dimetrodon\\frame11.png";
-	//
-	//std::string flow_output_path = "computed_flow.flo";
-	//std::string flow_img_path = "computed_img.png";
-	//
+	std::string first_img_path = "..\\..\\..\\resources\\eval-twoframes\\Dimetrodon\\frame10.png"; 
+	std::string second_img_path = "..\\..\\..\\resources\\eval-twoframes\\Dimetrodon\\frame11.png";
+	
+	std::string flow_output_path = "computed_flow.flo";
+	std::string flow_img_path = "computed_img.png";
+	
 
-	//std::string json_input_path = "temp_json_input.json";
-	//
-	////CommandLine Input
-	///*bool check_solve = console_ui::CheckCommandLineInput(argc, argv, first_img_path, second_img_path, flow_output_path, flow_img_path, json_input_path);
+	std::string json_input_path = "temp_json_input.json";
+	
+	//CommandLine Input
+	/*bool check_solve = console_ui::CheckCommandLineInput(argc, argv, first_img_path, second_img_path, flow_output_path, flow_img_path, json_input_path);
 
-	//if (check_solve == false)
-	//	return 1;*/
+	if (check_solve == false)
+		return 1;*/
 
-	//optflow_composition::ContainerInstaller di_installer;
+	optflow_composition::ContainerInstaller di_installer;
 
-	//auto options = std::make_shared<optflow_composition::ContainerOptions>();
+	auto options = std::make_shared<optflow_composition::ContainerOptions>();
 
-	////Parse JSON
-	//json_settings::JsonSetupSettings(json_input_path, options);
+	//Parse JSON
+	json_settings::JsonSetupSettings(json_input_path, options);
 
-	//options->SolverOptions->GNCSettings.GNCSettings->GNCSteps = 1;
+	options->SolverOptions->GNCSettings.GNCSettings->GNCSteps = 1;
 
-	//di_installer.SetOptions(options);
+	di_installer.SetOptions(options);
 
-	//auto di_container = di_installer.Install();
+	auto di_container = di_installer.Install();
 
-	//auto application = di_container->resolve<optflow_solvers::OpticalFlowApplication>();
+	auto application = di_container->resolve<optflow_solvers::OpticalFlowApplication>();
 
-	//application->ComputeOpticalFlow(first_img_path, second_img_path, flow_output_path, flow_img_path);
+	application->ComputeOpticalFlow(first_img_path, second_img_path, flow_output_path, flow_img_path);
 
 	/*
 	* TEMP JSON Output
 	*/
 
-	nlohmann::json out_json = json_settings::GenerateJSON();
+	//nlohmann::json out_json = json_settings::GenerateJSON();
 
-	json_settings::OutputJSON(out_json, "temp_json_input.json");
+	//json_settings::OutputJSON(out_json, "temp_json_input.json");
 
 	return 0;
 }
