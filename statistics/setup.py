858c27fc1eca0cf90916ca800f0fa99641908989
Arnizack <42911677+Arnizack@users.noreply.github.com>
2020-12-12 19:26:48 +0100

    optflow statistics folder structure

---
+++ statistics/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A subproject to evaluate the used modell and the implemented optical flow algorithm.',
+    author='Peer Drescher, Christian Libner',
+    license='',
+)
